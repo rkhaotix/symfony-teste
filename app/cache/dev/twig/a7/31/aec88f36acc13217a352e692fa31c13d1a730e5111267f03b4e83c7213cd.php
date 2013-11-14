beb3547c36e200d169f10cba3ce86906d544d241
Raphael A. Silva <rkhaotix@gmail.com>
2013-11-14 10:36:06 -0300

    Commit inicial

---
+++ app/cache/dev/twig/a7/31/aec88f36acc13217a352e692fa31c13d1a730e5111267f03b4e83c7213cd.php
@@ -0,0 +1,49 @@
+<?php
+
+/* BloggerBlogBundle::layout.html.twig */
+class __TwigTemplate_a731aec88f36acc13217a352e692fa31c13d1a730e5111267f03b4e83c7213cd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'sidebar' => array($this, 'block_sidebar'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 4
+    public function block_sidebar($context, array $blocks = array())
+    {
+        // line 5
+        echo "    Sidebar content
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle::layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 5,  28 => 4,);
+    }
+}
---
+++ app/cache/dev/twig/e8/63/06d1872f10cf1bd69bd3901e463e4d5a463f14b30bffc54e24b5ec3c15b9.php
@@ -0,0 +1,39 @@
+<?php
+
+/* BloggerBlogBundle:Default:index.html.twig */
+class __TwigTemplate_e86306d1872f10cf1bd69bd3901e463e4d5a463f14b30bffc54e24b5ec3c15b9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
